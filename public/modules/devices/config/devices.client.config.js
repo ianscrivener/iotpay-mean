d587de350fddb70545af634fa81c2c77bd4b8b53
Andrew <andrewjohnpitts@gmail.com>
2015-02-07 14:45:51 +1100

    Add devices CRUD again

--- public/modules/devices/config/devices.client.config.js
+++ public/modules/devices/config/devices.client.config.js
@@ -4,7 +4,7 @@
 angular.module('devices').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Devices', 'devices', 'dropdown', '/devices(/create)?');
+		Menus.addMenuItem('topbar', 'My Devices', 'devices', 'dropdown', '/devices(/create)?');
 		Menus.addSubMenuItem('topbar', 'devices', 'List Devices', 'devices');
 		Menus.addSubMenuItem('topbar', 'devices', 'New Device', 'devices/create');
 	}

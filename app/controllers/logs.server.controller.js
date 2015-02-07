02073843b82cd901dbb2225a98583de3f3a2050d
Andrew <andrewjohnpitts@gmail.com>
2015-02-08 10:48:22 +1100

    Debug data

--- app/controllers/logs.server.controller.js
+++ app/controllers/logs.server.controller.js
@@ -120,7 +120,7 @@ exports.delete = function(req, res) {
  * List of  logs
  */
 exports.list = function(req, res) { 
-  Log.find({device: req.device}).sort('-time').populate('device').limit(10).exec(function(err, logs) {
+  Log.find({device: req.device._id}).sort('-time').populate('device').limit(10).exec(function(err, logs) {
     if (err) {
       console.log(err);
       return res.status(400).send({

384408641ac2e10eeb21ca3e1dd6b306fdb65dd1
Andrew <andrewjohnpitts@gmail.com>
2015-02-08 11:44:41 +1100

    Change threshold

--- app/controllers/logs.server.controller.js
+++ app/controllers/logs.server.controller.js
@@ -54,7 +54,8 @@ exports.create = function(req, res) {
     } else {
 
       //Charge user logic - charge if the log percentage exceeded exceeds the threshold
-      if(log.percExceeded > device.config.thresholdLimit) {
+      if(log.percExceeded > 15) {
+      // if(log.percExceeded > device.config.thresholdLimit) {
         var data  = {amount:500, description: device.name};
         var customerData = {cardNumber: customer.cardNumber, expMonth: customer.expMonth, expYear: customer.expYear, cvc: customer.cvc, mobile: customer.mobile};
 

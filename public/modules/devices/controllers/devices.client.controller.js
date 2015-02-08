04812270a41de719dafe2c0a6275626e097693f8
Andrew <andrewjohnpitts@gmail.com>
2015-02-08 11:35:39 +1100

    Alternative refresh

--- public/modules/devices/controllers/devices.client.controller.js
+++ public/modules/devices/controllers/devices.client.controller.js
@@ -11,10 +11,7 @@ angular.module('devices').controller('DevicesController', ['$scope', '$http', '$
 		};
 
     $scope.refreshData = function() {
-      var tempDevices = Devices.query();      
-      tempDevices.forEach(function(tempDevice, i) {
-        $scope.devices[i].totalCharges = tempDevice.totalCharges;
-      });
+      $scope.devices = Devices.query();
     };
 
 		$scope.findOneLog = function() {
